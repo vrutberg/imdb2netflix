0245e7a613017bd4e08c6984da75440bb798c22b
Viktor Rutberg <wishie@gmail.com>
2014-06-04 21:50:45 +0200

    Now works better with TV shows /vrutberg

--- content_imdb.js
+++ content_imdb.js
@@ -1,8 +1,9 @@
 var title = document.querySelectorAll("h1.header")[0].innerText;
-chrome.extension.sendMessage(title, function(url) {
-	if(url) {
-		var link = '<a href="'+url+'" target="_blank">Watch Now!</a>';
-		var header = document.querySelectorAll("h1.header")[0];
-		header.innerHTML = header.innerHTML + link;
-	}
-});	
+
+chrome.extension.sendMessage(title, function (url) {
+  if (url) {
+    var link = '<a href="' + url + '" target="_blank">Watch Now!</a>';
+    var header = document.querySelectorAll("h1.header")[0];
+    header.innerHTML = header.innerHTML + link;
+  }
+});
--- imdb2netflix.js
+++ imdb2netflix.js
@@ -1,58 +1,53 @@
-var parseMovieTitle = function(orig) {
-	var title = orig.replace(/\s*?\(\d+\)\s*?$/, ""); // removes the year
-	var year = /\((\d+)\)/.exec(orig)[1];
+var parseTitle = function (orig) {
+  var title = orig.replace(/\s*?\(.+\)\s*?$/, ''); // removes the year
+  var year = /\(\s*(\d{4})[\s-]*(?:\d{4})?\)/.exec(orig.replace('–', '-'))[1];
 
-	return {
-		"title": title,
-		"year": year
-	};
+  return {
+    'title': title,
+    'year': year
+  };
 };
 
-var hasBeenCalled = false;
+chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
+  var url = details.url;
+  if (/^https?/.test(url) && /imdb\.com\/title/.test(url)) {
+    chrome.extension.onMessage.addListener(function (msg, sender, cb) {
+      if (typeof msg !== 'undefined') {
+        var title = parseTitle(msg);
+        var oauth = OAuthSimple().sign({
+          'path': 'http://api-public.netflix.com/catalog/titles',
+          'parameters': {
+            'term': title.title
+          },
+          'signatures': {
+            'consumer_key': 'm9emqnztega45aqp9xqswpmq',
+            'shared_secret': 'hn7NbaJ3u4'
+          }
+        });
 
-chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
-	var url = details.url;
-	if(/^https?/.test(url) && /imdb\.com\/title/.test(url)) {
-		chrome.extension.onMessage.addListener(function(msg, sender, cb) {
-			if(hasBeenCalled) return;
-			var hasBeenCalled = true;
+        jQuery.get(oauth.signed_url, function (data) {
+          var catalogTitle = undefined;
+          jQuery('catalog_title', data).each(function () {
+            var $this = $(this),
+              netflixTitle = $this.find('title').attr('short'),
+              netflixYear = $this.find('release_year').text();
 
-			if(msg) {
-				var movie = parseMovieTitle(msg);
-				var oauth = OAuthSimple().sign({
-					"path": "http://api-public.netflix.com/catalog/titles",
-					"parameters": {
-						"term": movie.title
-					},
-					"signatures": {
-						"consumer_key": "m9emqnztega45aqp9xqswpmq",
-						"shared_secret": "hn7NbaJ3u4"
-					}
-				});
+            if (netflixTitle === title.title && netflixYear === title.year) {
+              catalogTitle = this;
+              return false;
+            }
+          });
 
-				jQuery.get(oauth.signed_url, function(data) {
-					var catalogTitle = undefined;
-					jQuery("catalog_title", data).each(function() {
-						var $this = $(this),
-							title = $this.find("title").attr("short"),
-							release_year = $this.find("release_year").text();
+          if (typeof catalogTitle !== 'undefined') {
+            var url = $(catalogTitle).find('link[title="web page"]').attr('href');
+            cb(url);
+          }
+        });
+      }
 
-						if(title === movie.title && release_year === movie.year) {
-							catalogTitle = this;
-							return false;
-						}
-					});
+      return true;
+    });
 
-					if(catalogTitle !== undefined) {
-						var url = $(catalogTitle).find("link[title='web page']").attr("href");
-						cb(url);
-					}
-				});
-			}
-
-			return true;
-		});
-
-		chrome.tabs.executeScript(details.tabId, {"file": "content_imdb.js"});
-	}
+    chrome.tabs.executeScript(details.tabId, { 'file': 'content_imdb.js' });
+  }
 });
